0663763a0219e90f483b4fdd4a21459ea4745cfe
Andrey Matin <creohan@gmail.com>
2020-04-11 18:49:09 +0300

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,17 @@
 /**
- * Methods
+ * Load methods, helpers, polyfills etc.
  */
 import method from './module';
 
-document.addEventListener('DOMContentLoaded', function () {
+
+/**
+ * The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, 
+ * without waiting for stylesheets, images, and subframes to finish loading.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  
   method();
+  
 });
