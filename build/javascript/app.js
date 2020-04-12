ea6702237d1775aecaa272008f2b6334a0ffa24d
Andrey Matin <creohan@gmail.com>
2020-04-12 23:38:31 +0300

    Added package.json

--- build/javascript/app.js
+++ build/javascript/app.js
@@ -2,8 +2,15 @@
   'use strict';
 
   /**
-   * Methods
+   * Load methods, helpers, polyfills etc.
    */
+  /**
+   * The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, 
+   * without waiting for stylesheets, images, and subframes to finish loading.
+   *
+   * https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+   */
+
   document.addEventListener('DOMContentLoaded', function () {
   });
 
